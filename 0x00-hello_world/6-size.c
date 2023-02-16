296cbdb24822c3ee0e10bb23dacd9e2319c9da54
ElijahOpera <elijahopera@gmail.com>
2023-02-16 14:57:41 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - Entry of point
- * Return 0 (Success)
+ * main - Entry point
+ *
+ * Description: Size is not grandeur, and territory does not make a nation
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char a;
@@ -16,5 +21,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
 	return (0);
 }
