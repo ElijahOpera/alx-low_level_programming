b3cfebcfa888449d3fba7b9330ceb0d0965da93d
ElijahOpera <elijahopera@gmail.com>
2023-02-16 15:01:46 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-/*
- * main - Point of Entry
- * Return: 0 (Means Success)
+
+/**
+ * main - Entry point
+ *
+ * Description: Hello, puts
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
