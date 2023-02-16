6b38687881ea634984032ca05691b13e0bbdbae3
ElijahOpera <elijahopera@gmail.com>
2023-02-16 15:23:48 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: UNIX is basically a simple operating system
+ *
+ * Return: 1 (Success)
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+
+	return (1);
+}
