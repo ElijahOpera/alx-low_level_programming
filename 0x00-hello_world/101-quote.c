b799e7618f812ef2cca644fdd26fc6211bf6cd81
ElijahOpera <elijahopera@gmail.com>
2023-02-16 16:41:44 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
-#include <unixstd.h>
+#include <unistd.h>
+
 /**
  * main - Entry point
+ *
  * Description: UNIX is basically a simple operating system.
- * Retrun: 1 if Success
+ *
+ * Retrun: 1 if Success.
  */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	write("and that piece of art is useful - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
