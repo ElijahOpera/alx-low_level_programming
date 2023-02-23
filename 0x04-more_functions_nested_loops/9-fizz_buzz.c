50b4f107f2f80fefd6e4047a26748b082060db1d
ElijahOpera <elijahopera@gmail.com>
2023-02-23 10:47:48 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,7 +3,7 @@
 
 /**
  * main - prints Fizz Buzz each numbers of 3 and 5.
- * Return: Always 0.
+ * Return: Always 0 (Success).
  */
 
 int main(void)
@@ -28,11 +28,14 @@ int main(void)
 		{
 			printf("%d", n);
 		}
+
 		if (n != 100)
 		{
 			printf(" ");
 		}
 	}
+
 	printf("\n");
+
 	return (0);
 }
