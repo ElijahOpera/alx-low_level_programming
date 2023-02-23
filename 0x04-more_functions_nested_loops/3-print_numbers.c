8484e4202541ec9dd021de0f16d3259df1ad8a03
ElijahOpera <elijahopera@gmail.com>
2023-02-23 08:42:04 -0800

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 and 9.
+ * Return: no return.
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		_putchar(x);
+	}
+}
