dd6553a33415345993e9b36d3c225dc7479a5fb9
ElijahOpera <elijahopera@gmail.com>
2023-03-04 07:10:09 -0800

    4. strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: the string to search
+ * @accept: the set of bytes to search for
+ *
+ * Return: a pointer to the byte in s that matches one of the bytes in accept,
+ * or 0 if no such byte is found
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int x;
+
+	while (*s)
+	{
+		for (x = 0; accept[x]; x++)
+		{
+			if (*s == accept[x])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return ('\0');
+}
+
