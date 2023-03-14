9938ad6abec50bfbda3d7d7e0a5fa1457358bf9e
ElijahOpera <elijahopera@gmail.com>
2023-03-13 18:44:33 -0700

    1. The woman who has no imagination has no wings

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
- * *create_array - creates an array of chars,
- *  initialize it with a specific char.
- *  @c: char to initialize
- *  @size: number of bytes to allocate
+ * create_array - creates an array of chars,
+ * initialize it with a specific char.
+ * @c: char to initialize
+ * @size: number of bytes to allocate
  *
- *  Return: a pointer to the array or null if it fails
+ * Return: a pointer to the array or null if it fails
  */
 
 char *create_array(unsigned int size, char c)
